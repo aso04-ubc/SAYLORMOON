b3e0915008e05e3c6033d0dc42472646ca2b9d6f
ilovegrass123 <augustin.so@gmail.com>
2026-02-13 00:52:49 -0500

    Add expansion to 64 words, bugfixes

--- sha256.sv
+++ sha256.sv
@@ -1,17 +1,17 @@
 module sha256(
-    input logic clk,
-    input logic rst_n,
+    input logic clock,
+    input logic reset,
     input logic start,
     input logic [511:0] block,
     output logic [255:0] digest,
-    output logic done
+    output logic finish
 );
 
-logic [31:0] W [0:15];
+logic [31:0] W [0:63];
 
 function automatic logic [31:0] rotate_right(input logic[31:0] in, input int positions);
     // return {x[0], x[31:1]};
-    return (in >> positions) | (in << (31-positions));
+    return (in >> positions) | (in << (32-positions));
 endfunction
 
 function automatic logic [31:0] s0(input logic[31:0] in);
@@ -21,3 +21,9 @@ endfunction
 function automatic logic [31:0] s1(input logic [31:0] in);
     return (rotate_right(in, 17) ^ rotate_right(in, 19) ^ (in >> 10));
 endfunction
+
+for (int a = 16; a < 64; a++) begin
+    W[a] = s1(W[a-2]) + W[a-7] + s0(W[a-15]) + W[a-16];
+end
+
+endmodule
