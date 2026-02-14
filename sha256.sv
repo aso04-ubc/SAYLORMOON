b996f33a049a3089c4c8995f83559a2496a634d5
ilovegrass123 <augustin.so@gmail.com>
2026-02-14 16:24:36 -0500

    Incorporate both steps into single ff block

--- sha256.sv
+++ sha256.sv
@@ -7,8 +7,6 @@ module sha256(
     output logic finish
 );
 
-    logic [31:0] W [63:0];
-
     function automatic logic [31:0] rotate_right(input logic[31:0] in, input int positions);
         // return {x[0], x[31:1]};
         return (in >> positions) | (in << (32-positions));
@@ -23,11 +21,11 @@ module sha256(
     endfunction
 
     function automatic logic [31:0] choose(input logic [31:0] select, input logic [31:0] x, input logic [31:0] y);
-        return (x && select) ^ (y && ~select);
+        return (x & select) ^ (y & ~select);
     endfunction
 
     function automatic logic [31:0] majority (input logic [31:0] j, input logic [31:0] k, input logic [31:0] l);
-        return (j && k) ^ (j && l) ^ (k && l);
+        return (j & k) ^ (j & l) ^ (k & l);
     endfunction
 
     function automatic logic [31:0] S0 (input logic [31:0] in);
@@ -47,19 +45,10 @@ module sha256(
     //     end
     // end
 
-    logic [6:0] timer;
-    always_ff @(posedge clock || negedge reset) begin
-        if (!reset) timer <= 0;
-        else if (start) begin
-            for (int z = 0; z < 16; z++) begin
-                W[z] = block[511-b*32 +: 32];
-            end
-            timer <= 16;
-        end else if (timer >= 16 && timer < 64) begin
-            W[timer] = s1(W[timer-2]) + W[timer-7] + s0(W[timer-15]) + W[timer-16];
-            timer <= timer + 1;
-        end
-    end
+    
+    logic [31:0] temp1;
+    logic [31:0] temp2;
+    logic [31:0] W [63:0];
 
     logic [31:0] a, b, c, d, e, f, g, h;
     logic [31:0] H0, H1, H2, H3, H4, H5, H6, H7;
@@ -76,29 +65,65 @@ module sha256(
     0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
     };
 
+    // computer temp1 & temp2 in combinational logic to avoid pipeline hazard //
+    assign temp1 <= h + S1(e) + choose(e,f,g) + K[timer] + W[timer];
+    assign temp2 <= S0(a) + majority(a,b,c);
 
+    logic [6:0] timer;
+    always_ff @(posedge clock or negedge reset) begin
+        if (!reset) begin 
+            timer <= 0;
 
-    always_ff @(posedge clock || negedge reset) begin
-        if (!reset) begin
-            H0 <= 0x6a09e667
-            H1 <= 0xbb67ae85
-            H2 <= 0x3c6ef372
-            H3 <= 0xa54ff53a
-            H4 <= 0x510e527f
-            H5 <= 0x9b05688c
-            H6 <= 0x1f83d9ab
-            H7 <= 0x5be0cd19
+            H0 <= 32'h0x6a09e667;
+            H1 <= 32'h0xbb67ae85;
+            H2 <= 32'h0x3c6ef372;
+            H3 <= 32'h0xa54ff53a;
+            H4 <= 32'h0x510e527f;
+            H5 <= 32'h0x9b05688c;
+            H6 <= 32'h0x1f83d9ab;
+            H7 <= 32'h0x5be0cd19;
 
-            a <= 0x6a09e667
-            b <= 0xbb67ae85
-            c <= 0x3c6ef372
-            d <= 0xa54ff53a
-            e <= 0x510e527f
-            f <= 0x9b05688c
-            g <= 0x1f83d9ab
-            h <= 0x5be0cd19
+            a <= 32'h0x6a09e667;
+            b <= 32'h0xbb67ae85;
+            c <= 32'h0x3c6ef372;
+            d <= 32'h0xa54ff53a;
+            e <= 32'h0x510e527f;
+            f <= 32'h0x9b05688c;
+            g <= 32'h0x1f83d9ab;
+            h <= 32'h0x5be0cd19;
         end
+        else if (start) begin
+            for (int z = 0; z < 16; z++) begin
+                W[z] <= block[511-z*32 -: 32];
+            end
+            timer <= 16;
+        end else if (timer >= 16 && timer < 64) begin
 
+            h <= g;
+            g <= f;
+            f <= e;
+            e <= d + temp1;
+            d <= c;
+            c <= b;
+            b <= a;
+            a <= temp1 + temp2;
+
+            W[timer] <= s1(W[timer-2]) + W[timer-7] + s0(W[timer-15]) + W[timer-16];
+            timer <= timer + 1;
+        end else if (timer == 64) {
+            H0 <= H0 + a;
+            H1 <= H1 + b;
+            H2 <= H2 + c;
+            H3 <= H3 + d;
+            H4 <= H4 + e;
+            H5 <= H5 + f;
+            H6 <= H6 + g;
+            H7 <= H7 + h;
+
+            finish <= 1;
+        }
     end
+
+    assign digest = '{H0, H1, H2, H3, H4, H5, H6, H7};
 
 endmodule
