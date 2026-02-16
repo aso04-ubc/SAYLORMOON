2d8596900f18becc2634eb795f0594dc8cda44da
ilovegrass123 <augustin.so@gmail.com>
2026-02-15 19:45:29 -0500

    Change testbench and make module compatible with iverilog

--- generate_sha256.py
+++ generate_sha256.py
@@ -1,7 +1,8 @@
 import hashlib
+import random
 
-
-if __name__ == "__main__":
-    msg = b"hello world"
-    h = hashlib.sha256(msg).hexdigest()
-    print(h)
+with open("examples.txt","w") as f:
+    for _ in range(100):
+        msg = random.randbytes(64)
+        h = hashlib.sha256(msg).hexdigest()
+        f.write(msg.hex() + " " + h + "\n")
---
+++ tb_sha256.sv
@@ -0,0 +1,84 @@
+`timescale 1ns/1ps
+
+module tb_sha256;
+
+    logic clk = 0;
+    logic reset;
+    logic start;
+    logic [511:0] block;
+    logic [255:0] digest;
+    logic finish;
+    logic [255:0] expected;
+
+    always #5 clk = ~clk;
+
+    sha256 dut (
+        .clock(clk),
+        .reset(reset),
+        .start(start),
+        .block(block),
+        .digest(digest),
+        .finish(finish)
+    );
+
+    integer file;
+    integer scan_result;
+
+    initial begin
+        #1000000;
+        $display("Error: Simulation timed out");
+        $finish;
+    end
+
+    initial begin
+
+        $dumpfile("wave.vcd");
+        $dumpvars(0, tb_sha256);
+
+        start = 0;
+        block = 0;
+        expected = 0;
+
+        $display("Applying Reset...");
+        reset = 1; 
+        #20;
+        reset = 0; 
+        #10;
+
+        file = $fopen("examples.txt", "r");
+        if (file == 0) $fatal(1, "Open examples.txt failed");
+
+        while (!$feof(file)) begin
+
+            scan_result = $fscanf(file, "%h %h\n", block, expected);
+            
+            if (scan_result == 2) begin
+                
+                @(negedge clk);
+                start = 1;
+                
+                @(negedge clk);
+                start = 0;
+
+                wait(finish);
+                
+                @(posedge clk);
+
+                if (digest !== expected) begin
+                    $display("Error at time %0t", $time);
+                    $display("Block:    %h", block);
+                    $display("Got:      %h", digest);
+                    $display("Expected: %h", expected);
+                    $fatal(1, "Hash Mismatch!");
+                end else begin
+                    $display("Passed block %h...", block[511:480]);
+                end
+            end
+        end
+
+        $fclose(file);
+        $display("Passed");
+        $finish;
+    end
+
+endmodule
