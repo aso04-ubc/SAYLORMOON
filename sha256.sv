4bc1f08ac9b048c27f35f65f43350ac0c5195297
ilovegrass123 <augustin.so@gmail.com>
2026-02-15 19:46:17 -0500

    Fixed reset state change bug

--- sha256.sv
+++ sha256.sv
@@ -46,16 +46,28 @@ module sha256(
     logic [31:0] w_update;
     logic [6:0] timer;
 
-    localparam logic [31:0] K [0:63] = '{
-    32'h428a2f98, 32'h71374491, 32'hb5c0fbcf, 32'he9b5dba5, 32'h3956c25b, 32'h59f111f1, 32'h923f82a4, 32'hab1c5ed5,
-    32'hd807aa98, 32'h12835b01, 32'h243185be, 32'h550c7dc3, 32'h72be5d74, 32'h80deb1fe, 32'h9bdc06a7, 32'hc19bf174,
-    32'he49b69c1, 32'hefbe4786, 32'h0fc19dc6, 32'h240ca1cc, 32'h2de92c6f, 32'h4a7484aa, 32'h5cb0a9dc, 32'h76f988da,
-    32'h983e5152, 32'ha831c66d, 32'hb00327c8, 32'hbf597fc7, 32'hc6e00bf3, 32'hd5a79147, 32'h06ca6351, 32'h14292967,
-    32'h27b70a85, 32'h2e1b2138, 32'h4d2c6dfc, 32'h53380d13, 32'h650a7354, 32'h766a0abb, 32'h81c2c92e, 32'h92722c85,
-    32'ha2bfe8a1, 32'ha81a664b, 32'hc24b8b70, 32'hc76c51a3, 32'hd192e819, 32'hd6990624, 32'hf40e3585, 32'h106aa070,
-    32'h19a4c116, 32'h1e376c08, 32'h2748774c, 32'h34b0bcb5, 32'h391c0cb3, 32'h4ed8aa4a, 32'h5b9cca4f, 32'h682e6ff3,
-    32'h748f82ee, 32'h78a5636f, 32'h84c87814, 32'h8cc70208, 32'h90befffa, 32'ha4506ceb, 32'hbef9a3f7, 32'hc67178f2
-    };
+    // localparam logic [31:0] K [0:63] = '{
+    // 32'h428a2f98, 32'h71374491, 32'hb5c0fbcf, 32'he9b5dba5, 32'h3956c25b, 32'h59f111f1, 32'h923f82a4, 32'hab1c5ed5,
+    // 32'hd807aa98, 32'h12835b01, 32'h243185be, 32'h550c7dc3, 32'h72be5d74, 32'h80deb1fe, 32'h9bdc06a7, 32'hc19bf174,
+    // 32'he49b69c1, 32'hefbe4786, 32'h0fc19dc6, 32'h240ca1cc, 32'h2de92c6f, 32'h4a7484aa, 32'h5cb0a9dc, 32'h76f988da,
+    // 32'h983e5152, 32'ha831c66d, 32'hb00327c8, 32'hbf597fc7, 32'hc6e00bf3, 32'hd5a79147, 32'h06ca6351, 32'h14292967,
+    // 32'h27b70a85, 32'h2e1b2138, 32'h4d2c6dfc, 32'h53380d13, 32'h650a7354, 32'h766a0abb, 32'h81c2c92e, 32'h92722c85,
+    // 32'ha2bfe8a1, 32'ha81a664b, 32'hc24b8b70, 32'hc76c51a3, 32'hd192e819, 32'hd6990624, 32'hf40e3585, 32'h106aa070,
+    // 32'h19a4c116, 32'h1e376c08, 32'h2748774c, 32'h34b0bcb5, 32'h391c0cb3, 32'h4ed8aa4a, 32'h5b9cca4f, 32'h682e6ff3,
+    // 32'h748f82ee, 32'h78a5636f, 32'h84c87814, 32'h8cc70208, 32'h90befffa, 32'ha4506ceb, 32'hbef9a3f7, 32'hc67178f2
+    // };
+    reg [31:0] K [0:63];
+
+    initial begin
+    K[0] = 32'h428a2f98; K[0+1] = 32'h71374491; K[0+2] = 32'hb5c0fbcf; K[0+3] = 32'he9b5dba5; K[0+4] = 32'h3956c25b; K[0+5] = 32'h59f111f1; K[0+6] = 32'h923f82a4; K[0+7] = 32'hab1c5ed5;
+    K[8] = 32'hd807aa98; K[8+1] = 32'h12835b01; K[8+2] = 32'h243185be; K[8+3] = 32'h550c7dc3; K[8+4] = 32'h72be5d74; K[8+5] = 32'h80deb1fe; K[8+6] = 32'h9bdc06a7; K[8+7] = 32'hc19bf174;
+    K[16] = 32'he49b69c1; K[16+1] = 32'hefbe4786; K[16+2] = 32'h0fc19dc6; K[16+3] = 32'h240ca1cc; K[16+4] = 32'h2de92c6f; K[16+5] = 32'h4a7484aa; K[16+6] = 32'h5cb0a9dc; K[16+7] = 32'h76f988da;
+    K[24] = 32'h983e5152; K[24+1] = 32'ha831c66d; K[24+2] = 32'hb00327c8; K[24+3] = 32'hbf597fc7; K[24+4] = 32'hc6e00bf3; K[24+5] = 32'hd5a79147; K[24+6] = 32'h06ca6351; K[24+7] = 32'h14292967;
+    K[32] = 32'h27b70a85; K[32+1] = 32'h2e1b2138; K[32+2] = 32'h4d2c6dfc; K[32+3] = 32'h53380d13; K[32+4] = 32'h650a7354; K[32+5] = 32'h766a0abb; K[32+6] = 32'h81c2c92e; K[32+7] = 32'h92722c85;
+    K[40] = 32'ha2bfe8a1; K[40+1] = 32'ha81a664b; K[40+2] = 32'hc24b8b70; K[40+3] = 32'hc76c51a3; K[40+4] = 32'hd192e819; K[40+5] = 32'hd6990624; K[40+6] = 32'hf40e3585; K[40+7] = 32'h106aa070;
+    K[48] = 32'h19a4c116; K[48+1] = 32'h1e376c08; K[48+2] = 32'h2748774c; K[48+3] = 32'h34b0bcb5; K[48+4] = 32'h391c0cb3; K[48+5] = 32'h4ed8aa4a; K[48+6] = 32'h5b9cca4f; K[48+7] = 32'h682e6ff3;
+    K[56] = 32'h748f82ee; K[56+1] = 32'h78a5636f; K[56+2] = 32'h84c87814; K[56+3] = 32'h8cc70208; K[56+4] = 32'h90befffa; K[56+5] = 32'ha4506ceb; K[56+6] = 32'hbef9a3f7; K[56+7] = 32'hc67178f2;
+    end
 
     // anti pipeline hazard //
     assign temp1 = h + S1(e) + choose(e,f,g) + K[timer] + w_update;
@@ -73,7 +85,7 @@ module sha256(
     always_ff @(posedge clock or negedge reset) begin
         if (!reset) begin
 
-            timer <= 7'd65;
+            timer <= 7'd0;
             finish <= 0;
 
             H0 <= 32'h6a09e667;
